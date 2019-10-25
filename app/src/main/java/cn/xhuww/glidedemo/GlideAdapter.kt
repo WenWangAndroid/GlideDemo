386921aaad82417f4bef5cccb1e14055774b254a
王文 <xhuww2012@gmail.com>
2019-10-25 09:59:03 +0800

    完整示例

--- app/src/main/java/cn/xhuww/glidedemo/GlideAdapter.kt
+++ app/src/main/java/cn/xhuww/glidedemo/GlideAdapter.kt
@@ -4,6 +4,9 @@ import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -11,14 +14,18 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.bitmap.Rotate
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.CustomViewTarget
 import com.bumptech.glide.request.target.DrawableImageViewTarget
+import com.bumptech.glide.request.target.ImageViewTarget
 import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.bumptech.glide.signature.ObjectKey
 import kotlinx.android.synthetic.main.recycle_glide_item.view.*
-
+import java.io.File
+import java.io.IOException
 
 class GlideAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -28,7 +35,7 @@ class GlideAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int = 15
+    override fun getItemCount(): Int = 16
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val url =
@@ -67,10 +74,44 @@ class GlideAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 textView.text = "图片：常规加载"
             }
             1 -> {
-                Glide.with(context).load(url).placeholder(R.drawable.img_default).into(imageView)
+                Glide.with(context)
+                    .load(url)
+                    .placeholder(R.drawable.img_default)
+                    .signature(ObjectKey(System.currentTimeMillis()))
+                    .into(imageView)
                 textView.text = "图片：使用占位图"
             }
             2 -> {
+                val thumbnailUrl = url
+
+                Glide.with(context)
+                    .load(url)
+                    .thumbnail(Glide.with(context).load(thumbnailUrl))
+                    .into(imageView)
+                textView.text = "图片加载: 使用缩略图"
+            }
+            3 -> {
+                textView.text = "图片加载: 无过渡动画，点我加载图片"
+                textView.setOnClickListener {
+                    Glide.with(context)
+                        .load("http://www.nature-myview.com/uploads/2/8/3/6/28363143/168610_orig.jpg")
+                        .diskCacheStrategy(DiskCacheStrategy.NONE) //不使用磁盘缓存
+                        .skipMemoryCache(true) //不使用内存缓存
+                        .into(imageView)
+                }
+            }
+            4 -> {
+                textView.text = "图片加载: 过渡动画淡入淡出，点我加载图片"
+                textView.setOnClickListener {
+                    Glide.with(context)
+                        .load("http://www.nature-myview.com/uploads/2/8/3/6/28363143/168610_orig.jpg")
+                        .diskCacheStrategy(DiskCacheStrategy.NONE) //不使用磁盘缓存
+                        .skipMemoryCache(true) //不使用内存缓存
+                        .transition(DrawableTransitionOptions.withCrossFade(800))
+                        .into(imageView)
+                }
+            }
+            5 -> {
                 Glide.with(context)
                     .load(url)
                     .override(240, 240)
@@ -78,35 +119,35 @@ class GlideAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                     .into(imageView)
                 textView.text = "图片变换：指定图片宽高为240px"
             }
-            3 -> {
+            6 -> {
                 Glide.with(context)
                     .load(url)
                     .transform(Rotate(90))
                     .into(imageView)
                 textView.text = "图片变换：旋转90度"
             }
-            4 -> {
+            7 -> {
                 Glide.with(context)
                     .load(R.drawable.nezha_gif_01)
                     .transform(CircleCrop(), Rotate(90))
                     .into(imageView)
                 textView.text = "gif_01：图片变换 转换圆图，旋转90度 "
             }
-            5 -> {
+            8 -> {
                 Glide.with(context).load(R.drawable.nezha_gif_02).into(imageView)
                 textView.text = "gif_02: 默认无限循环播放"
             }
-            6 -> {
+            9 -> {
                 Glide.with(context).load(R.drawable.nezha_gif_03).listener(gifListener(1))
                     .into(imageView)
                 textView.text = "gif_03: 自定义播放次数 1次"
             }
-            7 -> {
+            10 -> {
                 Glide.with(context).load(R.drawable.nezha_gif_04)
                     .listener(gifListener(GifDrawable.LOOP_INTRINSIC)).into(imageView)
                 textView.text = "gif_04: 播放默认播放次数(3次)"
             }
-            8 -> {
+            11 -> {
                 Glide.with(context)
                     .load(R.drawable.nezha_gif_05)
                     .listener(gifListener(1))
@@ -120,26 +161,55 @@ class GlideAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                     }
                 }
             }
-            9->{
+            12 -> {
                 Glide.with(context)
                     .load(gifUrl)
                     .into(target)
                 textView.text = "图片加载: 使用Target 进度监听"
             }
-            10 -> {
+            13 -> {
                 Glide.with(context)
                     .load(gifUrl)
                     .signature(ObjectKey(System.currentTimeMillis()))
                     .into(target)
                 textView.text = "图片加载: 自定义缓存Key 为当前时间，区分相同URL"
             }
-            11 -> {
+            14 -> {
                 Glide.with(context)
                     .load("https://n.sinaimg.cn/tech/transform/438/w280h158/20190820/3ebc-icmpfxa4910530.gif")
                     .diskCacheStrategy(DiskCacheStrategy.NONE) //不使用磁盘缓存
                     .skipMemoryCache(true) //不使用内存缓存
                     .into(target)
                 textView.text = "图片加载: 使用Target进度监听,且不使用缓存"
+            }
+            15 -> {
+
+                Glide.with(context)
+                    .asFile()
+                    .load("https://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4")
+                    .listener(object : RequestListener<File> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<File>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: File?,
+                            model: Any?,
+                            target: Target<File>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            textView.text = "已文件加载：文件路径  ${resource?.absoluteFile}"
+                            return false
+                        }
+
+                    })
+                    .into(FileTarget(imageView))
             }
         }
     }
@@ -170,5 +240,17 @@ class GlideAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
 
+class FileTarget(@NonNull view: ImageView) : ImageViewTarget<File>(view) {
+    override fun setResource(resource: File?) {
+
+    }
+
+    override fun onLoadFailed(errorDrawable: Drawable?) {
+    }
+
+    override fun onResourceReady(@NonNull resource: File, @Nullable transition: Transition<in File>?) {
+
+    }
 }
--- app/src/main/java/cn/xhuww/glidedemo/GlideModule.kt
+++ app/src/main/java/cn/xhuww/glidedemo/GlideModule.kt
@@ -1,22 +1,32 @@
 package cn.xhuww.glidedemo
 
-//import android.content.Context
-//import com.bumptech.glide.Glide
-//import com.bumptech.glide.Registry
-//import com.bumptech.glide.annotation.GlideModule
-//import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
-//import com.bumptech.glide.module.AppGlideModule
-//import com.bumptech.glide.load.model.GlideUrl
-//import java.io.InputStream
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.load.model.GlideUrl
+import java.io.InputStream
+import com.bumptech.glide.load.engine.cache.LruResourceCache
+import com.bumptech.glide.GlideBuilder
+
 
 //@GlideModule
 //class OkHttpGlideModule : AppGlideModule() {
 //    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+//        //替换网络请求库
 //        registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory())
 //    }
 //
 //    override fun isManifestParsingEnabled(): Boolean {
 //        return true
+//    }
+//
+//    override fun applyOptions(context: Context, builder: GlideBuilder) {
+//        //设置内存缓存大小
+//        val memoryCacheSizeBytes = 1024 * 1024 * 20 // 20mb
+//        builder.setMemoryCache(LruResourceCache(memoryCacheSizeBytes.toLong()))
 //    }
 //}
 
